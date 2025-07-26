37f6e2a6f462f7a1d583b17bcea0f311e436b3fe
Daisentaur <thakur.darsh13@gmail.com>
2025-07-26 18:06:45 +0530

    Delete Study-Assistant directory

--- Study-Assistant/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://localhost:5000/upload"
-with open('1050-text-s.pdf', 'rb') as f:
-    response = requests.post(url, 
-        files={'files': f},
-        data={'keywords': 'sequence,geometric,mathematics'}
-    )
-print(response.json())
