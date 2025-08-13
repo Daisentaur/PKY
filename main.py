52acdd869299bc4de6632b6674b2807b16049b32
Codeinkardo107 <bhavyay107@gmail.com>
2025-08-14 00:57:19 +0530

    Update main.py
    Integration of the three pages

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import runpy 
+
+st.markdown("""
+    <style>
+    .stButton > button {
+        width: 100%;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+
+if "page" not in st.session_state:
+    st.session_state["page"] = "home"
+
+st.sidebar.title(" Menu")
+if st.sidebar.button(" Home"):
+    st.session_state["page"] = "home"
+if st.sidebar.button(" Client Page"):
+    st.session_state["page"] = "client"
+if st.sidebar.button(" Admin Page"):
+    st.session_state["page"] = "admin"
+
+
+
+if st.session_state["page"] == "home":
+    runpy.run_path("Home/Home_Page.py")
+elif st.session_state["page"] == "client":
+    runpy.run_path("Client/Client_Page.py")
+elif st.session_state["page"] == "admin":
+    runpy.run_path("Admin/Admin_Page.py")
